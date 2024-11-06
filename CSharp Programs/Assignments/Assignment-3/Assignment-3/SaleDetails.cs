e9873ed7dbb96a788073798e1d45cbcda65c3687
Sriteja Govindula <sritejag@infinite.com>
2024-11-06 21:33:20 +0530

    Assignment-3

---
+++ CSharp Programs/Assignments/Assignment-3/Assignment-3/SaleDetails.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    public class SaleDetails
+    {
+        static int Salesno;
+        static int Productno;
+        static float Price;
+        static DateTime dateofsale;
+        static int qty;
+        static int TotalAmount;
+        public SaleDetails(int salesno, int productno, float price, DateTime Dateofsale, int Qty)
+        {
+            Salesno = salesno;
+            Productno = productno;
+            Price = price;
+            dateofsale = Dateofsale;
+            qty = Qty;
+            Sales(qty, Price);
+        }
+        public void Sales(int qty, float Price)
+        {
+            TotalAmount = (int)(qty * Price);
+        }
+        public static void ShowData()
+        {
+            Console.WriteLine("The sales number is:" + Salesno);
+            Console.WriteLine("The product number is:" + Productno);
+            Console.WriteLine("The price number is:" + Price);
+            Console.WriteLine("The dateofsale number is:" + dateofsale);
+            Console.WriteLine("The quantity is:" + qty);
+        }
+
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the salesno:");
+            Salesno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the productno:");
+            Productno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the price:");
+            Price = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter the dateofsale:");
+            dateofsale = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter the qty:");
+            qty = Convert.ToInt32(Console.ReadLine());
+            
+
+
+            SaleDetails s = new SaleDetails(Salesno, Productno, Price, dateofsale, qty);
+            SaleDetails.ShowData();
+            Console.WriteLine("The total quantity is:" + TotalAmount);
+            Console.Read();
+        }
+    }
+}
--- CSharp Programs/Assignments/Assignment-3/Assignment-3/Student.cs
+++ CSharp Programs/Assignments/Assignment-3/Assignment-3/Student.cs
@@ -8,8 +8,8 @@ namespace Assignment_3
 {
     public class Student
     {
-        int rollno;
-        String name, Class, semester, branch;
+        static int rollno;
+        static String name, Class, semester, branch;
         int[] marks = new int[5];
         public Student(int Rollno, String Name, String class1, String Semester, String Branch)
         {
@@ -21,12 +21,92 @@ namespace Assignment_3
         }
         void GetMarks()
         {
+            Console.WriteLine("Enter the 5 marks:");
+            for(int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
+        int all_subjects = 0;
+        int average = 0;
+        void displayresult()
+        {
+            
+            for(int i = 0; i < marks.Length; i++)
+            {
+                all_subjects = all_subjects + marks[i];  // 0 + 45 -> 45
+                average = all_subjects / marks.Length; // calculating average.
+
+            }
+            int mark = 0, allsubjects = 0, Average = 0;
+            for(int i = 0; i < marks.Length; i++)
+            {
+                if (marks[i] < 35)
+                {
+                    mark = mark + 1;
+                }
+                else
+                {
+                    if (all_subjects > 35 && average < 50)
+                    {
+                        allsubjects = allsubjects + 1;
+                    }
+                    if (average > 50)
+                    {
+                        Average = Average + 1;
+                        
+                    }
+                }
+            }
+            if(mark > 1)
+            {
+                Console.WriteLine("The result is:: Fail");
+            }
+            else
+            {
+                if(allsubjects > 1)
+                {
+                    Console.WriteLine("The result is:: Fail");
+                }
+                else
+                {
+                    if(Average > 1)
+                    Console.WriteLine("The result is:: Pass");
+                }
+            }
 
         }
+        void DisplayData()
+        {
+            Console.WriteLine("The roll no is:" + rollno);
+            Console.WriteLine("The name is:" + name);
+            Console.WriteLine("The class is:" + Class);
+            Console.WriteLine("The semester is:" + semester);
+            Console.WriteLine("The branch is:" + branch);
+            displayresult();
+            Console.WriteLine("The average marks is:" + average);
+            Console.WriteLine("The total marks is:" + all_subjects);
+
+        }
+        
 
         public static void Main(String[] args)
         {
-
+            Console.WriteLine("Enter the rollnumber:");
+            rollno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the class:");
+            Class = Console.ReadLine();
+            Console.WriteLine("Enter the Semester:");
+            semester = Console.ReadLine();
+            Console.WriteLine("Enter the Branch:");
+            branch = Console.ReadLine();
+            Student s = new Student(rollno, name, Class, semester, branch);
+            s.GetMarks();
+            s.DisplayData();
+            Console.Read();
         }
     }
 }
