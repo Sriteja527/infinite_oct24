77dc8a4682f18c1d6a2cc78e891781a2ffaa8d5d
Sriteja Govindula <sritejag@infinite.com>
2024-11-10 18:54:42 +0530

    Assignment-4

---
+++ CSharp Programs/Assignments/Assignment-4/Assignment-4/ParttimeEmployee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    public class Employee
+    {
+        int Empid;
+        String Empname;
+        float Salary;
+        public Employee(int empid, String empname, float salary){
+            this.Empid = empid;
+            this.Empname = empname;
+            this.Salary = salary;
+            Console.WriteLine("Parent class constructor:");
+            Console.WriteLine("The Empid is:" + Empid);
+            Console.WriteLine("The Empname is:" + Empname);
+            Console.WriteLine("The Salary is:" + Salary);
+            Console.WriteLine("========================");
+        }
+
+
+    }
+    public class ParttimeEmployee : Employee
+    {
+        int Wages;
+        //Instantiating base class through the derived class Constructor.
+        public ParttimeEmployee(int empid, String empname, float salary) : base(empid, empname, salary)
+        {
+            Console.WriteLine("Derived class constructor:");
+        }
+        public static void Main(String[] args) {
+            int empid;
+            String empname;
+            float salary;
+            Console.WriteLine("Enter the employee id:");
+            empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the employee name:");
+            empname = (Console.ReadLine());
+            Console.WriteLine("Enter the employee salary:");
+            salary = Convert.ToSingle(Console.ReadLine());
+            ParttimeEmployee p = new ParttimeEmployee(empid, empname, salary);
+            Console.Read();
+
+
+
+
+        }
+}
+    }
+
---
+++ CSharp Programs/Assignments/Assignment-4/Assignment-4/Scholarship.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    public class Scholarship
+    {
+        
+        public double Merit()
+        {
+            double scholarship = 0;
+            Console.WriteLine("Enter the marks:");
+            int marks = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the fee:");
+            int fee = Convert.ToInt32(Console.ReadLine());
+            if(marks >= 70 && marks <= 80)
+            {
+                Console.WriteLine("The scholarship is:");
+                return (scholarship = fee * 0.20);
+                
+            }
+            else if(marks > 80 && marks <= 90)
+            {
+                Console.WriteLine("The scholarship is:");
+                return (scholarship = fee * 0.30);
+               
+            }
+            else if(marks > 90)
+            {
+                Console.WriteLine("The scholarship is:");
+                return (scholarship = fee * 0.50);
+                
+
+
+            }
+            else
+            {
+                Console.WriteLine("You are not elgible for scholarship:");
+            }
+            return scholarship;
+            
+            
+
+        }
+        public static void Main(String[] args)
+        {
+            Scholarship m = new Scholarship();
+            Console.WriteLine(m.Merit());
+            Console.Read();
+        }
+    }
+}
